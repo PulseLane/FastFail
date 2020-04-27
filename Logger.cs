d2419529465700035fe180def144a8eb254f634a
PulseLane <291593+PulseLane@users.noreply.github.com>
2020-04-27 02:21:06 -0700

    Initial commit

---
+++ Logger.cs
@@ -0,0 +1,11 @@
+﻿using IPALogger = IPA.Logging.Logger;
+using LogLevel = IPA.Logging.Logger.Level;
+
+namespace FastFail
+{
+    internal static class Logger
+    {
+        internal static IPALogger log { private get; set; }
+        internal static void Log(string message, LogLevel severity = LogLevel.Info) => log.Log(severity, message);
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,46 @@
+﻿using HarmonyLib;
+using IPA;
+using IPALogger = IPA.Logging.Logger;
+using LogLevel = IPA.Logging.Logger.Level;
+using System;
+using System.Reflection;
+
+namespace FastFail
+{
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    public class Plugin
+    {
+        internal static Harmony harmony;
+
+        [Init]
+        public void Init(IPALogger logger)
+        {
+            Harmony.DEBUG = true;
+            Logger.log = logger;
+            Logger.Log("Logging", LogLevel.Debug);
+        }
+
+        [OnStart]
+        public void OnStart()
+        {
+            harmony = new Harmony("com.PulseLane.BeatSaber.FastFail");
+            try
+            {
+                harmony.PatchAll(System.Reflection.Assembly.GetExecutingAssembly());
+                Logger.Log(":)");
+            }
+            catch (Exception ex)
+            {
+                Logger.Log($"Failed to apply harmony patches! {ex}", LogLevel.Error);
+            }
+
+            Logger.Log("TESTING!");
+        }
+
+        [OnExit]
+        public void OnExit()
+        {
+             harmony.UnpatchAll("com.PulseLane.BeatSaber.FastFail");
+        }
+    }
+}
