5efb9bf083e1725a2bd18f547be6e7f7dac07407
PulseLane <291593+PulseLane@users.noreply.github.com>
2020-05-02 11:34:01 -0700

    Enable autoskip by default

--- FastFail/Config.cs
+++ FastFail/Config.cs
@@ -3,12 +3,12 @@
     class Config
     {
         public static BS_Utils.Utilities.Config config = new BS_Utils.Utilities.Config("FastFail");
-        public static bool autoSkip = false;
+        public static bool autoSkip = true;
         public static bool enabled = true;
 
         public static void Read()
         {
-            autoSkip = config.GetBool("FastFail", "autoSkip", false, true);
+            autoSkip = config.GetBool("FastFail", "autoSkip", true, true);
             enabled = config.GetBool("FastFail", "enabled", true, true);
         }
 
